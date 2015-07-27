76d01d233f24828e2b409c4adc64aee77be2d1c8
Quang Hieu Vu <quang.vu@kustar.ac.ae>
2015-07-27 09:55:02 +0400

    update

--- manifests/classes/params.pp
+++ manifests/classes/params.pp
@@ -1,38 +1,18 @@
 class vormetric::params {
 
-  #default values of parameters for testing purpose
-  #$host_ip = "none"
-  #$host_dns = "none"
-  #$agent_download_url = "ec2-54-161-187-162.compute-1.amazonaws.com"
-  #$vm_dns = "none"
-  #$guardpoint_list = "none"  
-  #$status = "initial"
-  notice("come here")
-  
   # these are parameters to be retrieved from hiera
   if $appcara::params::site {
     $site_extsvc_option = $appcara::params::site["extension_service_option"]
     if $site_extsvc_option {
       $site_vormetric_option = $site_extsvc_option["vormetric"]
-      if $site_vormetric_option {
-        notice("site_vormetric_option exists")	  
-        $host_ip = "xx_${site_vormetric_option["host_ip"]}_xx"
-        $host_dns = "yy_${site_vormetric_option["host_dns"]}_yy"
-      }
-	  else{
-	    $host_ip = "site_vormetric_option_false"
-        $host_dns = "site_vormetric_option_false"		
+      if $site_vormetric_option {        
+	    $host_ip = "host_ip"
+		$host_dns = "host_dns"
+        #$host_ip = $site_vormetric_option["host_ip"]		
+        #$host_dns = $site_vormetric_option["host_dns"]
       }	  
-    }	
-    else{
-      $host_ip = "site_extsvc_option_false"
-      $host_dns = "site_extsrv_option_false"	  
-    }
-  }
-  else{
-    $host_ip = "appcara_params_site_false"
-	$host_dns = "appcara_params_site_false"
-  }
+    }	    
+  }  
   
   if $appcara::params::server {
     $svr_extsvc_option = $appcara::params::server["extension_service_option"]
