9ff8c3aab8d4689fba801af76d9b3596f7c17a79
Quang Hieu Vu <quang.vu@kustar.ac.ae>
2015-08-06 13:31:45 +0400

    update

--- manifests/classes/agent/linux/install.pp
+++ manifests/classes/agent/linux/install.pp
@@ -2,6 +2,10 @@ class vormetric::agent::linux::install() {
 
   $vm_management_folder = "/btconfig"
   $agent_download_url = "ec2-54-161-187-162.compute-1.amazonaws.com"
+  
+  file { "/bttest_vmstate_${vormetric::params::vm_state}":
+      ensure => directory, 
+  }
     
   if $vormetric::params::files_existed == "true" {
     
